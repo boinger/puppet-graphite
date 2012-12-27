d717ae940d5d196495e8935e628f9e4a415df47a
jv <jeff@jeffvier.com>
2012-12-27 13:13:33 -0800

    whitespace

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,23 +1,23 @@
 class graphite::params {
 	$build_dir = "/usr/local/src/"
-
+	
 	$graphiteVersion = "graphite-web-0.9.10"
-	$carbonVersion = "carbon-0.9.10"
-	$whisperVersion = "whisper-0.9.10"
-
-	$whisper_dl_url = "http://launchpad.net/graphite/0.9/0.9.10/+download/${whisperVersion}.tar.gz"
-	$whisper_dl_loc = "$build_dir/whisper.tar.gz"
-
-	$webapp_dl_url = "http://launchpad.net/graphite/0.9/0.9.10/+download/${graphiteVersion}.tar.gz"
-	$webapp_dl_loc = "$build_dir/graphite-web.tar.gz"
-
-	$carbon_dl_url = "http://launchpad.net/graphite/0.9/0.9.10/+download/${carbonVersion}.tar.gz"
-	$carbon_dl_loc = "$build_dir/carbon.tar.gz"
-
-	$install_prefix = "/opt/"
+	$carbonVersion   = "carbon-0.9.10"
+	$whisperVersion  = "whisper-0.9.10"
+	
+	$whisper_dl_url  = "http://launchpad.net/graphite/0.9/0.9.10/+download/${whisperVersion}.tar.gz"
+	$whisper_dl_loc  = "$build_dir/whisper.tar.gz"
+	
+	$webapp_dl_url   = "http://launchpad.net/graphite/0.9/0.9.10/+download/${graphiteVersion}.tar.gz"
+	$webapp_dl_loc   = "$build_dir/graphite-web.tar.gz"
+	
+	$carbon_dl_url   = "http://launchpad.net/graphite/0.9/0.9.10/+download/${carbonVersion}.tar.gz"
+	$carbon_dl_loc   = "$build_dir/carbon.tar.gz"
+	
+	$install_prefix  = "/opt/"
 
 	$apache_service_name = "httpd"
-	$web_user = "apache"
-	$graphitepkgs = []
+	$web_user            = "apache"
+	$graphitepkgs        = []
  
 }
